fac33068243c71a54be74cccb7ec24c01d95d650
August <2819763+Dangku@users.noreply.github.com>
2024-03-27 19:32:10 +0800

    fix incorrect file patch of quectel

--- bpi-bsp/bpi-cm4io/root/usr/local/bin/quectel-pppd.sh
+++ bpi-bsp/bpi-cm4io/root/usr/local/bin/quectel-pppd.sh
@@ -2,30 +2,30 @@
 
 #quectel-pppd devname apn user password
 echo "quectel-pppd options in effect:"
-QL_DEVNAME=/dev/ttyUSB3
-QL_APN=3gnet
-QL_USER=user
-QL_PASSWORD=passwd
+QL_DEVNAME=/dev/ttyUSB2
+QL_APN=ctnet
+QL_USER=ctnet@mycdma.cn
+QL_PASSWORD=vnet.mobi
 if [ $# -ge 1 ]; then
-	QL_DEVNAME=$1
+	QL_DEVNAME=$1	
 	echo "devname   $QL_DEVNAME    # (from command line)"
 else
 	echo "devname   $QL_DEVNAME    # (default)"
 fi
 if [ $# -ge 2 ]; then
-	QL_APN=$2
+	QL_APN=$2	
 	echo "apn       $QL_APN    # (from command line)"
 else
 	echo "apn       $QL_APN    # (default)"
 fi
 if [ $# -ge 3 ]; then
-	QL_USER=$3
+	QL_USER=$3	
 	echo "user      $QL_USER   # (from command line)"
 else
 	echo "user      $QL_USER   # (default)"
 fi
 if [ $# -ge 4 ]; then
-	QL_PASSWORD=$4
+	QL_PASSWORD=$4	
 	echo "password  $QL_PASSWORD   # (from command line)"
 else
 	echo "password  $QL_PASSWORD   # (default)"
